657eef8209499ad6281a26b14f9f85b0c254615c
Taiyu Yoshizawa <taiyu31@gmail.com>
2021-08-11 01:45:04 +0900

    init: Open Source Logo Maker

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from "@chakra-ui/react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <ChakraProvider>
+    <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+    </Head>
+    <Component {...pageProps} />
+  </ChakraProvider>
+    )
 }
 export default MyApp
